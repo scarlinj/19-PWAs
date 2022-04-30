2f34f87a6d65bb2cd15704548b8bdb2e8a75236a
Stephen Carlin <scarlinj@gmail.com>
2022-04-30 16:30:40 -0700

    optimized Javascript by splitting code into separate concerns

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,70 @@
-const path = require('path');
-const crypto = require("crypto");
 const webpack = require("webpack");
-const crypto_orig_createHash = crypto.createHash;
-crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
+// const crypto = require("crypto");
 
-module.exports = {
+// const crypto_orig_createHash = crypto.createHash;
+// crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+
+const config = {
     // entry is where webpack looks to start building the module
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js',
+    },
     // output used to tell webpack where files will go, what to name files 
     output: {
         // add hashFunction below to solve dependency issue
         // hashFunction: "xxhash64",
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        // in output, type filename: '[name].bundle.js'
+        // filename: 'app.bundle.js',
+        // filename: 'events.bundle.js',
+        // filename: 'schedule.bundle.js',
+        // filename: 'tickets.bundle.js',
+        filename: '[name].bundle.js',
+        // this will name the respective files the below:
+        // 'app.bundle.js',
+        // 'events.bundle.js',
+        // 'schedule.bundle.js',
+        // 'tickets.bundle.js',
+        path: path.resolve(__dirname, '/dist'),
+
+    },
+    module: {
+        rules: [{
+            // looking for jpg files - can change the below to add .png, .svg, etc.
+            test: /\.(jpe?g|png|gif)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                    name(file) {
+                        return "[path][name].[ext]"
+                    },
+                    publicPath: function (url) {
+                        return url.replace("../", "/assets/")
+                    },
+                }
+            }]
+        }]
     },
     plugins: [
         new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
+            $: 'jquery',
+            jQuery: 'jquery',
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static', // the report outputs to an HTML file called "report.html" in the dist folder - set value to"disable" to prevent automatically opening document
+        }),
+
     ],
     // mode is the environment that webpack uses - will have different features for different environments
     // development mode has hot reloading of webpack and debugging features
     mode: 'development',
+
 };
+
+// Put the above module.export into a variable "config" and module.export that variable
+module.exports = config;
